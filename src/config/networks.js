3b004b01c4bf0f781956be004fd3e177ec424f04
slydragonn <alogo18m@gmail.com>
2025-05-08 17:46:09 -0500

    add fuji testnet

---
+++ src/config/networks.js
@@ -0,0 +1,11 @@
+export const avalancheFuji = {
+  chainId: '0xA869', // Hexadecimal de 43113
+  chainName: 'Avalanche Fuji Testnet',
+  nativeCurrency: {
+    name: 'Avalanche',
+    symbol: 'AVAX',
+    decimals: 18
+  },
+  rpcUrls: ['https://api.avax-test.network/ext/bc/C/rpc'],
+  blockExplorerUrls: ['https://testnet.snowtrace.io/']
+};
